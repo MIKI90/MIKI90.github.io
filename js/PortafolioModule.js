09e365733008645cf954c941f2c685f5c31fcc88
MIKI90 <MOMO-ADA@mbp-de-miki.gateway.2wire.net>
2014-11-17 00:31:42 -0600

    Hashbangs
    Minor changes in angular js routing

--- js/PortafolioModule.js
+++ js/PortafolioModule.js
@@ -1,16 +1,24 @@
 var app = angular.module("portafolioModule",['portafolioModule','ngRoute']).config(['$locationProvider', '$routeProvider',
 function($locationProvider,$routeProvider){
   $routeProvider
-			.when('/about', {
+			.when('/About', {
 				templateUrl : '/templates/About.html',
 				controller : 'mainController'
 			})
-			.when('/contact', {
+			.when('/Project', {
 				templateUrl : '/templates/Project.html',
 				controller : 'mainController'
 			});
-  $locationProvider.hashPrefix('!');
-  $locationProvider.html5Mode(true);
+      .when('/Contact', {
+        templateUrl : '/templates/Contact.html',
+        controller : 'mainController'
+      }).
+      otherwise({
+        redirectTo: '/templates/About.html'
+      });
+  $locationProvider
+  .html5Mode(true)
+  .hashPrefix('!');
 }]);
 
 app.controller("mainController", function($scope){
