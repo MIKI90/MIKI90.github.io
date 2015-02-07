4c34fe10eba8c7dbf3fecd10d56fc9e1d000462b
MIKI90 <MOMO-ADA@mbp-de-miki.gateway.2wire.net>
2015-02-06 18:03:46 -0600

    Minor changes
    -Delete comments
    -Modify default UI route state

--- js/PortafolioModule.js
+++ js/PortafolioModule.js
@@ -1,10 +1,6 @@
 var app = angular.module("portafolioModule",['ui.router'])
 .config(function($stateProvider, $urlRouterProvider){
   $stateProvider
-  .state('home',{
-    url:'/',
-    templateUrl:''
-  })
   .state('About',{
     url:'/About',
     templateUrl:'templates/About.html'
@@ -17,35 +13,8 @@ var app = angular.module("portafolioModule",['ui.router'])
     url:'/Resources',
     templateUrl:'templates/Resources.html'
   });
-  $urlRouterProvider.otherwise('/');
+  $urlRouterProvider.otherwise('/About');
 });
-/*.config(['$locationProvider', '$routeProvider','$provide',
-  function($locationProvider,$routeProvider,$provide){
-    $provide.decorator('$sniffer', function($delegate) {
-    $delegate.history = false;
-      return $delegate;
-    });
-  $routeProvider
-			.when('/About', {
-				templateUrl : '/templates/About.html',
-				controller : 'mainController'
-			})
-			.when('/Project', {
-				templateUrl : '/templates/Project.html',
-				controller : 'mainController'
-			})
-      .when('/Resources', {
-        templateUrl : '/templates/Resources.html',
-        controller : 'mainController'
-      }).
-      otherwise({
-        redirectTo: '/templates/About.html'
-      });
-  $locationProvider
-  .html5Mode(true)
-  .hashPrefix('!');
-}]);*/
-
 app.controller("mainController", function($scope){
 
 });
